95a3a7551aae72a42972e2b9c2ef1b483d030d4b
Joe Snell <joe.snell@daveramsey.com>
2020-10-07 09:39:17 -0500

    cleanup

--- arn/parser.nim
+++ arn/parser.nim
@@ -46,8 +46,6 @@ proc parseArn*(arn: string): Arn =
 
     if components.len < 6:
         raise InvalidArn.newException("ARNs must have at least 6 components")
-    
-    # var resource, resourceName: string
 
     [
         prefix,
@@ -62,17 +60,6 @@ proc parseArn*(arn: string): Arn =
     if prefix != "arn":
         raise InvalidArn.newException("ARNs must start with 'arn'")
 
-    # if "/" in sixth:
-    #     var separated = sixth.split("/")
-    #     resource = separated[0]
-    #     resourceName = separated[1]
-    # elif ":" in sixth:
-    #     var separated = sixth.split(":")
-    #     resource = separated[0]
-    #     resourceName = separated[1]
-    # else:
-    #     resource = ""
-    #     resourceName = sixth
     var (resource, resourceName, sep) = parseResource(sixth, rest)
 
     result = Arn(
